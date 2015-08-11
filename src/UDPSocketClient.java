40ca8878df70b88d7fc6017d2915e3fef4cee738
wanhui <yao.wanhui@gmail.com>
2015-08-10 21:27:55 -0400

    modified:   src/UDPSocketClient.java 	modified:   src/UDPSocketServer.java

--- src/UDPSocketClient.java
+++ src/UDPSocketClient.java
@@ -2,35 +2,33 @@ import java.net.*;
 
 import tools.Item;
 import tools.ItemList;
-import tools.Product;
 import tools.channel.Channel;
 import tools.channel.Group;
 import tools.channel.NetworkIO;
-import tools.message.MessageProcesser;
-import tools.message.RetailerFEGetCatelogMessage;
-import tools.message.RetailerFESignInMessage;
 import tools.message.TestMessage;
 public class UDPSocketClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketTimeoutException {
     	try {
     		Channel channel = new Channel("UDPSocketServer", "UDPSocketClient", "localhost", 9876, Group.RetailerFE, null);
-    		//public Channel(String localProcessName, String peerProcessName, String peerHost, int peerPort, Group group, MessageProcesser messageProcesser){
 			NetworkIO networkIO = new NetworkIO(6789);
-//			networkIO.sendMsg(new RetailerFEGetCatelogMessage(channel.peerName, channel.localSeq, channel.peerSeq), channel.peerHost, channel.peerPort);
-//			networkIO.sendMsg(new RetailerFESignInMessage(channel.peerName, channel.localSeq, channel.peerSeq, 122, "password123"), channel.peerHost, channel.peerPort);
-//			networkIO.sendMsg(new TestMessage(channel.peerName, channel.localSeq, channel.peerSeq, new Product("Sony", "TV", (float) 120.0)), channel.peerHost, channel.peerPort);
-			
-			
+
 			ItemList itemList = new ItemList();
 			itemList.addItem(new Item("Sony", "TV", (float)120.0, 5));
 			itemList.addItem(new Item("Sony", "DVD", (float)220.0, 15));
 			itemList.addItem(new Item("Sony", "Camara", (float)320.0, 25));
 			
 			networkIO.sendMsg(new TestMessage(channel.peerProcessName, channel.localSeq, channel.peerSeq, itemList), channel.peerHost, channel.peerPort);
-			networkIO.receiveMessage();
+			while(true){
+				try {
+					networkIO.receiveMessage();
+					break;
+				} catch (SocketTimeoutException e) {
+					//time out 
+					//e.printStackTrace();
+				}
+			}
 			
 		} catch (SocketException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     }
--- src/UDPSocketServer.java
+++ src/UDPSocketServer.java
@@ -1,26 +1,32 @@
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 import tools.channel.Channel;
 import tools.channel.Group;
 import tools.channel.NetworkIO;
-import tools.message.RetailerFEGetCatelogMessage;
 import tools.message.RetailerFESignInMessage;
 public class UDPSocketServer {
-	
+
 	public static void main(String[] args) {
+		Channel channel = new Channel("UDPSocketClient", "UDPSocketServer", "localhost", 6789, Group.RetailerFE, null);
+		NetworkIO networkIO;
 		try {
-			Channel channel = new Channel("UDPSocketClient", "UDPSocketServer", "localhost", 6789, Group.RetailerFE, null);
-			NetworkIO networkIO = new NetworkIO(9876);
-			networkIO.receiveMessage();
-			
-//			networkIO.sendMsg(new RetailerFEGetCatelogMessage(channel.peerName, channel.localSeq, channel.peerSeq), channel.peerHost, channel.peerPort);
-			networkIO.sendMsg(new RetailerFESignInMessage(channel.peerProcessName, channel.localSeq, channel.peerSeq, 1234567, "password_abc"), channel.peerHost, channel.peerPort);
+			networkIO = new NetworkIO(9876);
+			while(true){
+				try {
+					networkIO.receiveMessage();
+					break;
+				} catch (SocketTimeoutException e) {
+					//time out 
+					//e.printStackTrace();
+				}
+			}
+			networkIO.sendMsg(new RetailerFESignInMessage(channel.peerProcessName, channel.localSeq
+					, channel.peerSeq, 1234567, "password_abc"), channel.peerHost, channel.peerPort);
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
 }
 
