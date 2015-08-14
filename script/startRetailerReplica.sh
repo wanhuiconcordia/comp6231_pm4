04a3eabf8ed2cd52e409cef1d34feb6f9c9d47c4
wanhui <yao.wanhui@gmail.com>
2015-08-14 18:26:22 -0400

    modified:   .gitignore 	modified:   script/startRetailerReplica.sh 	deleted:    script/start_client.sh 	deleted:    script/start_logger_server.sh 	deleted:    script/start_retailer.sh 	deleted:    script/start_retailer_sequencer.sh 	modified:   settings/settings.conf 	deleted:    settings/start_client.sh 	deleted:    settings/start_logger_server.sh 	deleted:    settings/start_manufacturer.sh 	deleted:    settings/start_retailer.sh 	deleted:    settings/start_warehouse.sh 	deleted:    settings/start_warehouseClient.sh 	script/startClient.sh 	script/startLogServer.sh 	script/startRetailerFE.sh 	script/startRetailerSequencer.sh

--- script/startRetailerReplica.sh
+++ script/startRetailerReplica.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java retailer.RetailerReplica $1 $2&
+java -cp ".:./libs/dom4j-2.0.0-ALPHA-2.jar:./libs/swingx-1.6.jar:" retailer.RetailerReplica $1 $2
 #(exec java retailer.RetailerReplica "$@" > /tmp/retailerReplica.log 2>&1 )&
